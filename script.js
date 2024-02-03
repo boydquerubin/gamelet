029980cb224d9d4e5cff330a0a0312ce29f287f0
Boyd Querubin <boydisaacq@gmail.com>
2024-02-02 22:54:57 -0700

    update

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-const ball = document.getElementById('ball');
-document.addEventListener('keydown', handleKeyPress);
-let position = 0;
+const ball = document.getElementById("ball"); // get the ball
+document.addEventListener("keydown", handleKeyPress); // listen for keys
+let position = 0; //set initial position
+
+// handleKeyPress responds to certain key presses by updating position.
 
 function handleKeyPress(e) {
-    if (e.code === 'ArrowLeft') {
-        position = position - 10;
-    }
-    if (e.code === 'ArrowRight') {
-        position = position + 10;
-    }
-    if (position < 0) {
-        position = 0;
-    }
-    refresh();
-}
-function refresh() {
-    ball.style.left = position + 'px';
+  if (e.code === "ArrowLeft") {
+    position = position - 10;
+  }
+  if (e.code === "ArrowRight") {
+    position = position + 10;
+  }
+  if (position < 0) {
+    position = 0;
+  }
+  refresh(); // this updates the ball's position
 }
 
+// this function changes the position of the ball
+function refresh() {
+  ball.style.left = position + "px";
+}
